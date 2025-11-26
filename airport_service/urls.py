d683bffadc6ea1126ab2547764254f416c2f342b
unknown <kononovb777@gmail.com>
2025-11-26 11:39:13 +0200

    create user

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("__debug__/", include(debug_toolbar.urls)),
+    path("user/", include("user.urls", namespace="user")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
